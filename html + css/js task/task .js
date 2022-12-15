f85a165592b57aeb26701cb997705db1ce0a0997
ahmedbnfahmy <120669562+ahmedbnfahmy@users.noreply.github.com>
2022-12-15 19:17:26 +0200

    initial commit

---
+++ html + css/js task/task .js
@@ -0,0 +1,28 @@
+// function sum(num1,num2){
+
+//     return num1+num2 ;
+
+// };
+
+
+// function clicked(){
+//     var num1 = parseInt(prompt("inter your first num...."));
+//     var num2 = parseInt(prompt("inter your second num...."));
+//     console.log(sum(num1,num2))
+// };
+
+
+// let a = 100 ,b=30;
+//  x= a+b;
+//  (x<100)?alert(true):alert(false);
+
+
+function change(){
+    let x= parseInt(prompt("inter your num...."));
+    return x;
+}
+console.log (change());
+
+
+
+
